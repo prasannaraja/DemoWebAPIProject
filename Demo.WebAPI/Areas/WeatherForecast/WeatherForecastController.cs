8b6ffc41c97afda36a5cdb22dcf94bfdae66d125
Prasanna Raja <prasannaraja@msn.com>
2022-03-28 20:25:56 +0200

    Create WeatherForecastController.cs

---
+++ Demo.WebAPI/Areas/WeatherForecast/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Demo.WebAPI.Controllers;
+
+namespace Demo.WebAPI.Areas.WeatherForecast.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : BaseController
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<Demo.Core.Domain.WeatherForecast.WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Demo.Core.Domain.WeatherForecast.WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
