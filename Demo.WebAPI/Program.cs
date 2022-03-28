9df24b19bededf08595cfa13784806b4200de566
Prasanna Raja <prasannaraja@msn.com>
2022-03-28 20:47:10 +0200

    Update Program.cs

--- Demo.WebAPI/Program.cs
+++ Demo.WebAPI/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -9,6 +11,24 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseDeveloperExceptionPage();
+app.UseExceptionHandler(a => a.Run(async (context) => await context.Response
+.WriteAsJsonAsync(new {
+    error = context.Features.Get<IExceptionHandlerPathFeature>().Error.Message
+})));
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demp API - v1");
+    c.OAuthClientId(builder.Configuration["AzureAd:ClientId"]);
+    c.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+    if (!builder.Environment.IsDevelopment())
+        c.RoutePrefix = String.Empty;
+});
+app.UseCors("default");
+//app.UseMiddleware<ErrorHandlerMiddleware>();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -17,10 +37,31 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.Use(async (context, next) => {
+    if (!context.User.Identity?.IsAuthenticated ?? false)
+    {
+        context.Response.StatusCode = 401;
+        await context.Response.WriteAsync("Not Authenticated");
+    }
+    else await next();
+});
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "WeatherForecast",
+        pattern: "{area:exists}/{controller=WeatherForecast}/{action=list}"
+    );
+
+    endpoints.MapControllerRoute(
+        name:"default",
+        pattern:"{controller=Token}/{action=Get}"
+    );
+});
 
 app.Run();
 
